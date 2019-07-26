17ca3c279f178766426307158d3a305bfcd0af17
yancheng911 <1390077343@qq.com>
2019-07-26 23:07:25 +0800

    小demo

---
+++ src/main/java/com/ssm/demo/controller/Test.java
@@ -0,0 +1,29 @@
+package com.ssm.demo.controller;
+
+import java.util.Scanner;
+
+public class Test {
+
+    private int id;
+    private  String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        int a=new Scanner(System.in).nextInt();
+    }
+}
